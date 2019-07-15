dd7dc64757d23aa0c4e6aed6192e407a02bd2a94
WilliamJunior88 <43940372+WilliamJunior88@users.noreply.github.com>
2019-07-14 21:59:26 -0300

    Update 04 - Insert_Select.sql

---
+++ 04 - Insert_Select.sql
@@ -0,0 +1,23 @@
+--insert into Escolas values ('58817222117001', 'Primeira Escola', '2134568967', 'Rua Faz de Conta', 130, null, 'Rio de Janeiro', 'Rio de Janeiro', 'Atendimento@PrimeiraEscola.com.br');
+--insert into Escolas values ('71117316132003', 'Segunda Escola', '2133578569', 'Rua Faz de Conta', 210, null, 'Rio de Janeiro', 'Rio de Janeiro', 'Atendimento@SegundaEscola.com.br');
+
+--insert into Escolas values ('58817222117001', 'Terceira Escola', '2134568967', 'Rua Faz de Conta', 130, null, 'Rio de Janeiro', 'Rio de Janeiro', 'Atendimento@TerceiraEscola.com.br');
+--insert into Escolas values ('71117316132003', 'Quarta Escola', '2133578569', 'Rua Faz de Conta', 210, null, 'Rio de Janeiro', 'Rio de Janeiro', 'Atendimento@QuartaEscola.com.br');
+
+--insert into Escolas values ('58817222117001', 'Quinta Escola', '2134568967', 'Rua Faz de Conta', 130, null, 'Rio de Janeiro', 'Rio de Janeiro', 'Atendimento@QuintaEscola.com.br');
+--insert into Escolas values ('71117316132003', 'Sexta Escola', '2133578569', 'Rua Faz de Conta', 210, null, 'Rio de Janeiro', 'Rio de Janeiro', 'Atendimento@SextaEscola.com.br');
+
+select * from [dbo].[Escolas]
+
+--insert into Turmas values (1, 101, 'Turma Matematica', 'Noite', 25);
+--insert into Turmas values (1, 201, 'Turma Portugues', 'Tarde', 20);
+--insert into Turmas values (2, 302, 'Turma Ingles', 'Manha', 15);
+--insert into Turmas values (2, 402, 'Turma Espanhol', 'Manha', 15);
+
+--insert into Turmas values (3, 101, 'Turma Matematica', 'Noite', 30);
+--insert into Turmas values (4, 201, 'Turma Portugues', 'Tarde', 25);
+--insert into Turmas values (5, 302, 'Turma Ingles', 'Manha', 20);
+--insert into Turmas values (6, 402, 'Turma Espanhol', 'Manha', 20);
+
+
+select * from [dbo].[Turmas]
